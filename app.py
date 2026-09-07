f1e8ad30c266cb054fa629f1426f85fdb5f3ec2b
vboxuser <vboxuser@Cheoimd.home>
2026-09-07 16:47:49 +0000

    eee

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
-print("Hello world")
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"Hello depuis mon conteneur Docker !")
+
+server = HTTPServer(("0.0.0.0", 5000), Handler)
+
+print("Serveur demarre sur le port 5000")
+server.serve_forever()
+
